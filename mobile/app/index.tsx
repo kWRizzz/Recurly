ed2477e909d8126d7480d04b37f22b60280b5cc6
kWRizzz <krishnathey2j@gmail.com>
2026-04-20 22:47:31 +0530

    setup done

---
+++ mobile/app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+
+      </Text>
+      <View
+        className=" bg-amber-900"
+      >
+        <Text>
+            asdsad
+        </Text>
+      </View>
+    </View>
+  );
+}
