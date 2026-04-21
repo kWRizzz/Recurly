1b71ce59ab7107e1464545700b14eafa135c34a0
kWRizzz <krishnathey2j@gmail.com>
2026-04-21 22:03:39 +0530

    routing architecture done

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -3,18 +3,11 @@ import { Text, View } from "react-native";
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white text-white">
+    <View className="flex-1 items-center justify-center bg-amber-700   text-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        
 
       </Text>
-      <View
-        className=" bg-amber-900"
-      >
-        <Text>
-            asdsad
-        </Text>
-      </View>
       <Link
         href={"/(auth)/signIn"}
         className=" bg-black  py-2.5 px-4 rounded-2xl text-white mt-2 "
--- mobile/app/(tabs)/subscriptions/[id].tsx
+++ mobile/app/(tabs)/subscriptions/[id].tsx
@@ -4,13 +4,26 @@ import { Link } from 'expo-router'
 
 const SubscriptiionHandle = () => {
   return (
-    <View>
-      <Text>SubscriptiionHandle</Text>
+    <View
+      className=' flex-1  items-center bg-amber-700 '
+    >
+
+      <Text
+        className=' text-5xl font-bold'
+      >
+        Subscriptions 
+      </Text>
+
+      <View>
+
+        
+      </View>
       {/* <Link
         href={"/"}
       >
         Go Back
       </Link> */}
+    
     </View>     
   )
 }
