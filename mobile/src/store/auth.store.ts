b7ec70065a06fa2202a2e5bb90a0699a07219ccd
kWRizzz <krishnathey2j@gmail.com>
2026-06-01 23:36:59 +0530

    frontend services and zustand implemented

---
+++ mobile/src/store/auth.store.ts
@@ -0,0 +1,19 @@
+import{
+    create
+} from "zustand"
+
+interface AuthState{
+    token:string | null;
+
+    setToken:(
+        token:string | null
+    )=>void
+}
+
+
+export const userAuthStore= create<AuthState>((set)=>({
+    token:null,
+    setToken:(token)=>set({token}),
+}))
+
+
