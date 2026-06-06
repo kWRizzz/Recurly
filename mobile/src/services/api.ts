21bc57293d7936ec13d38524e2c06ceadeca611f
kWRizzz <krishnathey2j@gmail.com>
2026-06-06 18:22:16 +0530

    frontend bug fix in custom hooks and updated api axios

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,21 @@
 import axios from "axios";
 
+import {
+    userAuthStore
+}from "../store/auth.store"
+
 export const api= axios.create({
     baseURL:"http://localhost:3000/api"
 })
+
+api.interceptors.request.use(
+    (config)=>{
+
+        const token = userAuthStore.getState().token
+
+        if(token){
+            config.headers.Authorization=  `Bearer ${token}`
+        }
+        return config;
+    }
+)
