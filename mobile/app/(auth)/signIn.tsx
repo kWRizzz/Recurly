f31e2a2f85fbee10a4a5ea64c7674cdddf42731d
kWRizzz <krishnathey2j@gmail.com>
2026-06-06 16:00:36 +0530

    frontend register services and component done

--- mobile/app/(auth)/signIn.tsx
+++ mobile/app/(auth)/signIn.tsx
@@ -1,19 +1,133 @@
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TouchableOpacity
+}
+  from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import {
+  Controller,
+  useForm
+} from "react-hook-form"
+import {
+  zodResolver
+} from "@hookform/resolvers/zod"
+import AppGUI from '@/src/components/ui/AppGUI'
+import {
+  registerFormData,
+  registerSchema
+} from '@/src/validation/auth.validation'
 
 const signIn = () => {
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<registerFormData>({
+    resolver: zodResolver(registerSchema)
+  })
+
+  const onSubmit = (
+    data: registerFormData
+  ) => {
+    console.log(data);
+  }
+
+
   return (
     <View
-        className=' flex-1 justify-center items-center bg-yellow-100'
+      className=' flex-1 justify-center px-6 bg-white'
     >
-      <Text>DOnt have an accoutn</Text>
-    <Link
+      <Text
+        className=' text-4xl font-bold mb-2'
+      >
+        Create Account
+      </Text>
+      <Text
+        className=' text-gray-500 mb-8 '
+      >
+        Register to continue
+      </Text>
+
+      <Controller
+        control={control}
+        name='name'
+        render={({
+          field: {
+            onChange,
+            value
+          }
+        }) => (
+          <AppGUI
+            placeholder="Name"
+            value={value}
+            onChangeText={onChange}
+            error={
+              errors?.name?.message
+            }
+
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name='email'
+        render={({
+          field: {
+            onChange,
+            value
+          }
+        }) => (
+          <AppGUI
+            placeholder="Email"
+            value={value}
+            onChangeText={onChange}
+            error={
+              errors?.email?.message
+            }
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name='password'
+        render={({
+          field: {
+            onChange,
+            value
+          }
+        }) => (
+          <AppGUI
+            placeholder='Passowrd'
+            value={value}
+            onChangeText={onChange}
+            error={
+              errors?.password?.message
+            }
+          />
+        )}
+      />
+      <TouchableOpacity
+        onPress={handleSubmit(onSubmit)}
+        className='bg-blue-500 p-4 rounded-xl mt-2'
+      >
+        <Text
+          className=' text-center text-white font-semibold'
+        >
+          Register
+        </Text>
+      </TouchableOpacity>
+
+      <Link
         href={"/(auth)/signUp"}
-        className=" bg-black  py-2.5 px-4 rounded-2xl text-white mt-2 "
-    >
-        SingUP
-    </Link>
+        className=' text-center mt-6'
+      >
+        Already A User Click Here
+      </Link>
     </View>
   )
 }
--- mobile/src/validation/auth.validation.ts
+++ mobile/src/validation/auth.validation.ts
@@ -8,4 +8,12 @@ export const loginSchema= z.object({
     password:z.string().min(4,"pass must be atleast of 4 chars")
 })
 
+export const registerSchema= z.object({
+    name:z.string().min(3),
+    email:z.email().trim(),
+    password:z.string().trim()
+})
+
 export type loginFromData= z.infer<typeof loginSchema>
+
+export type registerFormData= z.infer<typeof registerSchema>
