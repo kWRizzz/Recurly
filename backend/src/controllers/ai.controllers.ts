85d194690c342d6e4c1217bdfdde94628744f256
kWRizzz <krishnathey2j@gmail.com>
2026-06-08 03:05:39 +0530

    updated routes at the backend part

--- backend/src/controllers/ai.controllers.ts
+++ backend/src/controllers/ai.controllers.ts
@@ -1,34 +1,85 @@
-import{
+import {
     Response
-}from "express"
-import{
+} from "express"
+import {
     CustomRequest
 } from "../types/index"
 import notesModel from "../models/notes.model"
+import { askQuestion, generateQuiz } from "../services/gemini.service"
 
 
-const getQuize= async (
-    req:CustomRequest,
-    res:Response
-):Promise<void> => {
+export const getQuize = async (
+    req: CustomRequest,
+    res: Response
+): Promise<void> => {
     try {
         const note = await notesModel.findOne({
-            _id:req.params.noteId,
-            user:req.user?._id
+            _id: req.params.noteId,
+            user: req.user?._id
         })
 
-        if(!note){
+        if (!note) {
             res.status(201).json({
-                message:"User nhi mila",
-                success:false
+                message: "User nhi mila",
+                success: false
             });
             return;
         }
+
+        const quiz= await generateQuiz(
+            note.content
+        )
+
+        res.status(200).json({
+            message:"your quiz submintted",
+            success:true,
+            quiz
+        })
     } catch (error) {
         console.log(`cant generate quiz sorr :( ${error}`);
         res.status(500).json({
-            message:`cant gen qunbbiz ${error}`,
-            success:false
+            message: `cant gen qunbbiz ${error}`,
+            success: false
+        })
+    }
+}
+
+export const chatWithNotes = async (
+    req: CustomRequest,
+    res: Response
+): Promise<void> => {
+    try {
+        const { question } = req.body;
+
+        const note = await notesModel.findById({
+            _id: req.params.noteId,
+            user: req.user?._id
+        })
+
+        if (!note) {
+            res.status(200).json({
+                message: "no notes found on your account ",
+                success: false
+            });
+            return;
+        }
+
+        const answer = await askQuestion(
+            note.content,
+            question
+            
+        )
+
+        res.status(200).json({
+            message:"question aswered",
+            success: true,
+            answer,
+        });
+    } catch (error) {
+        console.log(`some error occured in talking ${error}`);
+        res.status(500).json({
+            message: `some error occured in talking ${error}`,
+            success: false
         })
     }
 }
--- backend/src/routes/pdf.route.ts
+++ backend/src/routes/pdf.route.ts
@@ -7,9 +7,25 @@ import { processPdf } from "../controllers/pdf.controllers"
 import {
     uploads
 } from "../middleware/multer"
+import{
+    chatWithNotes,
+    getQuize
+} from "../controllers/ai.controllers"
 
 const router= Router()
 
 router.post("/pdfupload",protect,uploads.single("pdf"),processPdf)
+
+router.post(
+    "/quiz/:noteId",
+    protect,
+    getQuize
+)
+
+router.post(
+    "/chat/:noteId",
+    protect,
+    chatWithNotes
+)
 
 export default router
